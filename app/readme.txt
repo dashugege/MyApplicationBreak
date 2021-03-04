218babf4ffa3ac702cbae3bbacb2c5c3a35365fc
hongjiang <hongjiang@soyoung.com>
2021-03-04 21:51:55 +0800

    init

---
+++ app/readme.txt
@@ -0,0 +1,10 @@
+
+
+使用 Androidstudio 自带的 minidump_stackwalk 工具 ，来解析dump文件
+
+ 路径 /Applications/Android\ Studio.app/Contents/bin/lldb/bin/minidump_stackwalk
+
+
+命令  ./minidump_stackwalk /Users/hongjiang/Desktop/da857634-9849-4a6c-4d5ed088-fd0cfedb.dmp
+
+
---
+++ breakpad/src/main/java/com/soyoung/breakpad/BreakpadInit.java
@@ -0,0 +1,15 @@
+package com.soyoung.breakpad;
+
+public class BreakpadInit {
+
+    static {
+        System.loadLibrary("breakpad-core");
+    }
+
+    public static void initBreakpad(String path){
+        initBreakpadNative(path);
+    }
+
+    private static native void initBreakpadNative(String path);
+
+}
